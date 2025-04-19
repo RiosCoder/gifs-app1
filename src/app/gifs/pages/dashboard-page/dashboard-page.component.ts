2c664057c643a0710bb4bcb96a497db0f52650a1
RiosCoder <edson.rios2010@gmail.com>
2025-04-19 19:45:15 -0400

    Adicion de components para separar el dashboard

--- src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [
+    RouterOutlet
+  ],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.css'
 })
-export class DashboardPageComponent {
+export default class DashboardPageComponent {
 
 }
