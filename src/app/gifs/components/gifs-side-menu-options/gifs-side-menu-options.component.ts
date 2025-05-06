48e8c724cd65d962dab2337d5ee3aa2cc350151d
RiosCoder <edson.rios2010@gmail.com>
2025-05-05 20:24:28 -0400

    Adicion de componentes router

--- src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
 
+interface MenuOpcion {
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
+
 @Component({
   selector: 'gifs-side-menu-options',
   imports: [],
   templateUrl: './gifs-side-menu-options.component.html',
-  styleUrl: './gifs-side-menu-options.component.css'
+  styleUrl: './gifs-side-menu-options.component.css',
 })
 export class GifsSideMenuOptionsComponent {
-
+  menuOptions: MenuOpcion[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      route: 'Gifs populares',
+      subLabel: '/dashboard/trending',
+    },
+    {
+      icon: 'fa-solid fa-magnifyng-glass',
+      label: 'Buscador',
+      route: 'Buscar gifs',
+      subLabel: '/dashboard/search',
+    },
+  ];
 }
