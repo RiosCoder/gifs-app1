0024a657833259d2517c111756ff03ee8830bccc
Illitory <edsonrios2010@gmail.com>
2025-05-07 22:08:10 -0400

    Adicion de lista de imagenes

--- src/app/gifs/pages/trending/trending.component.ts
+++ src/app/gifs/pages/trending/trending.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { GifListComponent } from "../../components/gif-list/gif-list.component";
 
 @Component({
   selector: 'app-trending',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './trending.component.html',
   styleUrl: './trending.component.css'
 })
